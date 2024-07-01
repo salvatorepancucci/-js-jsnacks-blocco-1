763bbac2bd7930f3e63e14047f923c8154a88586
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-01 15:43:29 +0200

    jsnack1: main condition within for iteration using flag

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -3,3 +3,12 @@ const invitati = ["Nick Carraway", "Jay Gatsby", "Daisy Buchanan", "Tom Buchanan
 
 // Chiedi all'utente il suo nome
 const nomeUtente = prompt("Qual è il tuo nome?");
+
+// Controlla se il nome è nella lista degli invitati
+let invitato = false;
+for (let i = 0; i < invitati.length; i++) {
+    if (invitati[i].toLowerCase() === nomeUtente.toLowerCase()) {
+        invitato = true;
+        break;
+    }
+}
