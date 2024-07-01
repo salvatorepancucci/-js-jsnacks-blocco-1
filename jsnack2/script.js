c45b6da803ac25e6a3e8d679f4dd72683770a4c6
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-01 15:50:04 +0200

    jsnack2: user input via prompt, converting string to number, pushing the odd one to the array

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,2 +1,10 @@
 // Crea un array vuoto
 const numeriDispari = [];
+
+// Chiedi per 6 volte all'utente di inserire un numero
+for (let i = 0; i < 6; i++) {
+    const numero = parseInt(prompt(`Inserisci il numero ${i + 1} di 6:`));
+    if (numero % 2 !== 0) {
+        numeriDispari.push(numero);
+    }
+}
