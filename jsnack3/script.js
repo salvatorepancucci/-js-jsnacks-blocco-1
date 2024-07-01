3896131b87fdd92e298010c68fde5a18cf51a61f
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-01 15:54:02 +0200

    jsnack3: calculated input sum with for iteration converting string to numbers

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,9 @@
 // Chiedi un numero di 4 cifre all'utente
 const numeroQuattroCifre = prompt("Inserisci un numero di 4 cifre:");
 
+// Calcola la somma di tutte le cifre che compongono il numero
+let sommaCifre = 0;
+for (let i = 0; i < numeroQuattroCifre.length; i++) {
+    sommaCifre += parseInt(numeroQuattroCifre[i]);
+}
+
