9391e414c7e8e6f00b00da6b0e18f07c2aebf254
salvatorepancucci <salvatore.pancucci@gmail.com>
2024-07-01 15:42:00 +0200

    jsnack1: user input through prompt

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,2 +1,5 @@
 // Array con i nomi degli invitati
 const invitati = ["Nick Carraway", "Jay Gatsby", "Daisy Buchanan", "Tom Buchanan", "Jordan Baker", "Myrtle Wilson"];
+
+// Chiedi all'utente il suo nome
+const nomeUtente = prompt("Qual è il tuo nome?");
